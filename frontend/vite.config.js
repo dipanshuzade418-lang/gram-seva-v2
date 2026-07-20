f8bde94f2756c24ae5672fa71777a6c8934c4d21
dipanshuzade418-lang <dipanshuzade418@gmail.com>
2026-07-20 13:45:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/gram arogaya v2/',
+  base:'/gram-seva-v2/',
   server: {
     port: 5173,
     proxy: {
